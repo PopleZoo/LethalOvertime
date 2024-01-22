088614ee8a99254146ffc7a256541d1057c28f19
Bailley <94534377+PopleZoo@users.noreply.github.com>
2024-01-22 03:54:34 +0000

    Added references and UI to screen, need to set initial positioning though

---
+++ LethalOvertime/Patches/HudManagerPatch.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Linq;
+using HarmonyLib;
+using LethalOvertime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Object = UnityEngine.Object;
+
+namespace ShipLoot.Patches
+{
+    [HarmonyPatch]
+    internal class HudManagerPatcher
+    {
+        private static GameObject _totalCounter;
+        private static TextMeshProUGUI _textMesh;
+        private static float _displayTimeLeft;
+        private const float DisplayTime = 5f;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HUDManager), "PingScan_performed")]
+        private static void Start(HUDManager __instance, InputAction.CallbackContext context)
+        {
+            if (!StartOfRound.Instance.inShipPhase && !GameNetworkManager.Instance.localPlayerController.isInHangarShipRoom)
+                return;
+
+            if (!_totalCounter)
+                CopyValueCounter();
+            var tempbonus = TimeOfDay.Instance.quotaFulfilled - TimeOfDay.Instance.profitQuota;
+            float value = tempbonus <= 0 ? 0 : (tempbonus / 5 + 15 * TimeOfDay.Instance.daysUntilDeadline);
+            _textMesh.text = "Potential overtime bonus:" + value;
+            _displayTimeLeft = DisplayTime;
+
+            if (!_totalCounter.activeSelf)
+                GameNetworkManager.Instance.StartCoroutine(ShipLootCoroutine());
+        }
+
+        private static IEnumerator ShipLootCoroutine()
+        {
+            _totalCounter.SetActive(true);
+
+            while (_displayTimeLeft > 0f)
+            {
+                float time = _displayTimeLeft;
+                _displayTimeLeft = 0f;
+                yield return new WaitForSeconds(time);
+            }
+
+            _totalCounter.SetActive(false);
+        }
+        private static void CopyValueCounter()
+        {
+            GameObject valueCounter = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddle/ValueCounter");
+            if (!(bool)(UnityEngine.Object)valueCounter)
+                LethalOvertimeModBase.Instance.mls.LogError("Failed to find ValueCounter object to copy!");
+
+            _totalCounter = UnityEngine.Object.Instantiate<GameObject>(valueCounter.gameObject, valueCounter.transform.parent, false);
+            Vector3 localPosition = _totalCounter.transform.localPosition;
+
+            // Adjust the position higher up and more to the right
+            float adjustedX = Mathf.Clamp(localPosition.x + LethalOvertimeModBase.AdjustScreenPositionXaxis.Value + 80f, 0f, Screen.width);
+            float adjustedY = Mathf.Clamp(localPosition.y + LethalOvertimeModBase.AdjustScreenPositionYaxis.Value - 150f, 0f, Screen.height);
+
+            _totalCounter.transform.localPosition = new Vector3(adjustedX, adjustedY, localPosition.z);
+
+            _textMesh = _totalCounter.GetComponentInChildren<TextMeshProUGUI>();
+            _textMesh.fontSizeMin = 5f;
+            _textMesh.fontSize = LethalOvertimeModBase.FontSize.Value;
+            _textMesh.ForceMeshUpdate();
+
+            // Set the anchor and pivot of the text's RectTransform to the top right of the parent
+            _textMesh.alignment = TextAlignmentOptions.TopRight;
+            RectTransform textRectTransform = _textMesh.rectTransform;
+            textRectTransform.anchorMin = new Vector2(1f, 1f); // Change the anchorMin to (1, 1) to position the child at the top right of the parent
+            textRectTransform.anchorMax = new Vector2(1f, 1f); // Change the anchorMax to (1, 1) to position the child at the top right of the parent
+            textRectTransform.pivot = new Vector2(1f, 1f); // Change the pivot to (1, 1) to position the child at the top right of the parent
+
+            // Adjust local position for top right corner
+            Vector3 textLocalPosition = textRectTransform.localPosition;
+            textRectTransform.localPosition = new Vector3(textLocalPosition.x - 20f, textLocalPosition.y, textLocalPosition.z);
+        }
+
+
+    }
+}
---
+++ LethalOvertime/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+
+namespace LethalOvertime
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalOvertimeModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "PopleZoo.LethalOvertime";
+        private const string modName = "Lethal Overtime";
+        private const string modVersion = "0.0.0.1";
+        private readonly Harmony harmony = new Harmony(modGUID);
+        public static LethalOvertimeModBase Instance;
+        public ManualLogSource mls;
+
+
+        public static ConfigEntry<float> AdjustScreenPositionXaxis;
+        public static ConfigEntry<float> AdjustScreenPositionYaxis;
+        public static ConfigEntry<string> TextColorHex;
+        public static ConfigEntry<float> FontSize;
+
+
+        private void Awake()
+        {
+            if (LethalOvertimeModBase.Instance == null) { LethalOvertimeModBase.Instance = this; };
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Plugin LethalOvertime is loaded!");
+            this.LoadConfigs();
+            harmony.PatchAll();
+        }
+        private void LoadConfigs()
+        {
+            LethalOvertimeModBase.FontSize = Config.Bind("Settings", "FontSize", 20f, "The default font size is 20, to make/see this change you may have to do this manually in the config file itself in the bepinex folder");
+            LethalOvertimeModBase.TextColorHex = Config.Bind("Settings", "TextColorHex", "#78FFAE", "The default text color for LethalOvertime text, value must be a hexadecimal");
+            LethalOvertimeModBase.AdjustScreenPositionXaxis = Config.Bind("Settings", "AdjustScreenPositionXaxis", 0f, "The default value is 0, you will add or take away from its original position");
+            LethalOvertimeModBase.AdjustScreenPositionYaxis = Config.Bind("Settings", "AdjustScreenPositionYaxis", 0f, "The default value is 0, you will add or take away from its original position");
+        }
+    }
+}
